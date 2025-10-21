a99098364c948e60bf366783070bac703d296add
Calascibetta Romain <romain.calascibetta@gmail.com>
2025-10-21 11:13:02 +0200

    Fix some sources and forks about dns-resolver

--- source.sh
+++ source.sh
@@ -4,7 +4,7 @@ opam pin add -yn --ignore-pin-depends git+https://github.com/robur-coop/utcp.git
 opam pin add -yn --ignore-pin-depends git+https://git.robur.coop/robur/mnet.git#with-tls
 opam pin add -yn --ignore-pin-depends git+https://git.robur.coop/robur/mkernel.git
 opam pin add -yn --ignore-pin-depends git+https://github.com/dinosaure/mirage-crypto.git#miou-solo5
-opam pin add -yn --ignore-pin-depends git+https://github.com/dinosaure/ca-certs-nss.git#mkernel
+opam pin add -yn --ignore-pin-depends git+https://github.com/mirage/mirage-ptime.git#fix-compilation-without-solo5-context
 
 [ ! -d "vendors" ] && mkdir vendors
 [ ! -d "vendors/bstr" ] && opam source bstr --dir vendors/bstr
@@ -18,3 +18,4 @@ opam pin add -yn --ignore-pin-depends git+https://github.com/dinosaure/ca-certs-
 [ ! -d "vendors/dns-server" ] && opam source dns-server --dir vendors/dns-server
 [ ! -d "vendors/tls" ] && opam source tls --dir vendors/tls
 [ ! -d "vendors/ca-certs-nss" ] && opam source ca-certs-nss --dir vendors/ca-certs-nss
+[ ! -d "vendors/mirage-ptime" ] && opam source mirage-ptime --dir vendors/mirage-ptime
